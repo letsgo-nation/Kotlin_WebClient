55e48c902486fef206521be480353920cf834078
letsgo-nation <ssowon528@gmail.com>
2024-01-01 11:34:42 +0900

    BlogService에 WebClient로 카카오 API 호출

--- src/main/kotlin/com/won/study/blog/service/BlogService.kt
+++ src/main/kotlin/com/won/study/blog/service/BlogService.kt
@@ -11,7 +11,25 @@ import org.springframework.web.reactive.function.client.bodyToMono
 
 @Service
 class BlogService {
+
     fun searchKakao(blogDto: BlogDto): String? {
-        return "SearchKakao"
+        val webClient: WebClient = WebClient
+                .builder()
+                .baseUrl("https://dapi.kakao.com")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build()
+        val response = webClient
+                .get()
+                .uri { it.path("/v2/search/blog")
+                        .queryParam("query", blogDto.query)
+                        .queryParam("sort", blogDto.sort)
+                        .queryParam("page", blogDto.page)
+                        .queryParam("size", blogDto.size)
+                        .build() }
+                .header("Authorization", "KakaoAK 93d2da970f3cfb2e2cab759c1554afe1")
+                .retrieve()
+                .bodyToMono<String>()
+        val result = response.block()
+        return result
     }
 }
